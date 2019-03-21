d0de027d3719010fb5593f3ecae14cf4613c8a70
xiongronggui <419934330@qq.com>
2019-03-21 15:43:34 +0800

    Add files via upload

---
+++ src/main/java/com/panda/rabbitmq/common/ResultGenerator.java
@@ -0,0 +1,25 @@
+package com.panda.rabbitmq.common;
+
+public class ResultGenerator
+{
+    private  static final String DEFAULT_SUCCESS_MESSAGE = "SUCCESS";
+
+    public static Result genSuccessResult(){
+        return new Result()
+                .setCode(Result.ResultCode.SUCCESS)
+                .setMessage(DEFAULT_SUCCESS_MESSAGE);
+    }
+
+    public static  Result genSuccessResult(Object data){
+        return new Result()
+                .setCode(Result.ResultCode.SUCCESS)
+                .setMessage(DEFAULT_SUCCESS_MESSAGE)
+                .setData(data);
+    }
+
+    public  static  Result genFailResult(String message){
+        return new Result()
+                    .setCode(Result.ResultCode.FAIL)
+                    .setMessage(message);
+    }
+}
