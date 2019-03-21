cb110316215a3ce87514c3c7dccc7da314d44c85
xiongronggui <419934330@qq.com>
2019-03-21 15:44:56 +0800

    Create GlobalNewsRepository.java

---
+++ src/main/java/com/panda/rabbitmq/Repository/GlobalNewsRepository.java
@@ -0,0 +1,8 @@
+package com.panda.rabbitmq.Repository;
+
+import com.panda.rabbitmq.model.GlobalNews;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GlobalNewsRepository extends CrudRepository<GlobalNews,Long>{
+
+}
